deebd3c9e5f2589304e52d3046fd89ee20490630
Carmine Noviello <carmine.noviello@airqnetworks.com>
2015-06-01 15:59:40 +0200

    Removed uneeded comments inside board configuration files

--- stmhal/boards/STM32F4NUCLEO/mpconfigboard.h
+++ stmhal/boards/STM32F4NUCLEO/mpconfigboard.h
@@ -4,47 +4,23 @@
 #define MICROPY_HW_MCU_NAME         "STM32F401xE"
 
 #define MICROPY_HW_HAS_SWITCH       (1)
-#define MICROPY_HW_HAS_SDCARD       (0)
-#define MICROPY_HW_HAS_MMA7660      (0)
-#define MICROPY_HW_HAS_LIS3DSH      (0) //MODIFICATO
-#define MICROPY_HW_HAS_LCD          (0)
-#define MICROPY_HW_ENABLE_RNG       (0) //MODIFICATO
 #define MICROPY_HW_ENABLE_RTC       (1)
-#define MICROPY_HW_ENABLE_TIMER     (0) //MODIFICATO
-#define MICROPY_HW_ENABLE_SERVO     (0)
-#define MICROPY_HW_ENABLE_DAC       (0) //MODIFICATO
-#define MICROPY_HW_ENABLE_SPI1      (0)
-#define MICROPY_HW_ENABLE_SPI2      (0)
-#define MICROPY_HW_ENABLE_SPI3      (0)
-#define MICROPY_HW_ENABLE_CAN       (0) //MODIFICATO
 
-// HSE is 8MHz
+// HSI is 16MHz
 #define MICROPY_HW_CLK_PLLM (16)
 #define MICROPY_HW_CLK_PLLN (336)
 #define MICROPY_HW_CLK_PLLP (RCC_PLLP_DIV4)
 #define MICROPY_HW_CLK_PLLQ (7)
 
 // UART config
-// #define MICROPY_HW_UART1_PORT (GPIOA)
-// #define MICROPY_HW_UART1_PINS (GPIO_PIN_9 | GPIO_PIN_10)
 #define MICROPY_HW_UART2_PORT (GPIOA)
 #define MICROPY_HW_UART2_PINS (GPIO_PIN_2 | GPIO_PIN_3)
-#define MICROPY_HW_UART2_RTS  (GPIO_PIN_1)
-#define MICROPY_HW_UART2_CTS  (GPIO_PIN_0)
-// #define MICROPY_HW_UART3_PORT (GPIOD)
-// #define MICROPY_HW_UART3_PINS (GPIO_PIN_8 | GPIO_PIN_9)
-// #define MICROPY_HW_UART3_RTS  (GPIO_PIN_12)
-// #define MICROPY_HW_UART3_CTS  (GPIO_PIN_11)
-// #define MICROPY_HW_UART4_PORT (GPIOA)
-// #define MICROPY_HW_UART4_PINS (GPIO_PIN_0 | GPIO_PIN_1)
 #define MICROPY_HW_UART6_PORT (GPIOC)
 #define MICROPY_HW_UART6_PINS (GPIO_PIN_6 | GPIO_PIN_7)
 
 // I2C busses
 #define MICROPY_HW_I2C1_SCL (pin_B6)
 #define MICROPY_HW_I2C1_SDA (pin_B7)
-// #define MICROPY_HW_I2C2_SCL (pin_B10)
-// #define MICROPY_HW_I2C2_SDA (pin_B11)
 
 // USRSW is pulled low. Pressing the button makes the input go high.
 #define MICROPY_HW_USRSW_PIN        (pin_C13)
@@ -53,7 +29,7 @@
 #define MICROPY_HW_USRSW_PRESSED    (0)
 
 // LEDs
-#define MICROPY_HW_LED1             (pin_A5) // red
+#define MICROPY_HW_LED1             (pin_A5) // Green LD2 LED on Nucleo
 #define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
 #define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
 #define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
@@ -61,8 +37,3 @@
 // USB config
 #define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
 #define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
-
-// // SD card detect switch
-// #define MICROPY_HW_SDCARD_DETECT_PIN        (pin_A8)
-// #define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
-// #define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
